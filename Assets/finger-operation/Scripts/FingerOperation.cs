43423edf9fe8b03c46477e0fd077a134628e0c5a
drumath <2237k.drumath@gmail.com>
2020-10-20 17:25:36 +0900

    :sparkles: ちょっとスクリプト書いてみた

---
+++ Assets/finger-operation/Scripts/FingerOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace FingerOperations
+{
+    [ExecuteAlways]
+    [RequireComponent(typeof(Animator))]
+    public class FingerOperation: MonoBehaviour
+    {
+        private Animator _animator;
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+    }
+}
