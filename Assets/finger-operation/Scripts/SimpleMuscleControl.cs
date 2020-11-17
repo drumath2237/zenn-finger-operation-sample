f4d63a2ebf0cbb00b4042760717d8dc2da16c9f7
drumath <2237k.drumath@gmail.com>
2020-11-17 19:04:06 +0900

    :sparkles: 別にmuscle制御用スクリプトを書いた

---
+++ Assets/finger-operation/Scripts/SimpleMuscleControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace FingerOperations
+{
+    [RequireComponent(typeof(Animator))]
+    [ExecuteAlways]
+    public class SimpleMuscleControl : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+
+        // 左腕の曲げ具合
+        [SerializeField, Range(-1.0f, 1.0f)] private float _leftArmStretched;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            if (_animator != null)
+            {
+                _animator = GetComponent<Animator>();
+            }
+            
+            var handler = new HumanPoseHandler(_animator.avatar, _animator.transform);
+            var humanpose = new HumanPose();
+            handler.GetHumanPose(ref humanpose);
+
+            // index=42はHumanTraitで、Left Forarm Stretchという名前で定義してあるmuscleです。
+            humanpose.muscles[42] = Mathf.Clamp(_leftArmStretched, -1.0f, 1.0f);
+            
+            handler.SetHumanPose(ref humanpose);
+        }
+    }
+}
