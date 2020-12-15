a6f96b90de89f5f16f7ea94b3fcf3ba3b609b3cf
drumath <2237k.drumath@gmail.com>
2020-12-16 00:57:41 +0900

    :sparkles: HumanoidTestを追加

---
+++ Assets/finger-operation/Scripts/HumanoidTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace FingerOperations
+{
+    [RequireComponent(typeof(Animator))]
+    public class HumanoidTest : MonoBehaviour
+    {
+        private Animator _animator;
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            
+            _animator.GetBoneTransform(HumanBodyBones.LeftUpperArm)
+                .Rotate(new Vector3(0,0,60f));
+        }
+    }
+}
